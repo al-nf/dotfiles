64404836aa87e13a8ebc50f8d29e03d73e7559b5
Alan Fung <alankjfung@gmail.com>
2025-08-15 15:52:29 -0700

    rofi

--- rofi.nix
+++ rofi.nix
@@ -90,5 +90,59 @@ in
       }
 
       textbox {
-        padding
+        padding          : 4px 8px;
+        background-color : @bg2;
+      }
+
+      listview {
+        padding      : 4px 0px;
+        lines        : 12;
+        columns      : 1;
+        scrollbar    : true;
+        fixed-height : false;
+        dynamic      : true;
+      }
+
+      element {
+        padding : 4px 8px;
+        spacing : 8px;
+      }
+
+      element normal urgent {
+        text-color: @urgent;
+      }
+
+      element normal active {
+        text-color: @accent;
+      }
+
+      element alternate active {
+        text-color: @accent;
+      }
+
+      element selected {
+        text-color       : @bg1;
+        background-color : @accent;
+      }
+
+      element selected urgent {
+        background-color: @urgent;
+      }
+
+      element-icon {
+        size: 0.8em;
+      }
+
+      element-text {
+        text-color: inherit;
+      }
+
+      scrollbar {
+        handle-width : 4px;
+        handle-color : @fg2;
+        padding      : 0 4px;
+      }
+    '';
+  };
+}
 
